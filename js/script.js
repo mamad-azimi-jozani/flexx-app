1ecba2f37f6ec94bc55f3cd3fec038910dd20c64
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-04-25 13:17:21 +0330

    complete movie detail

--- js/script.js
+++ js/script.js
@@ -29,9 +29,9 @@ async function displayPopularMovies(){
     const url = `https://api.themoviedb.org/3/movie/popular?`
     const { results } = await fetchData(url)
     const popular = document.querySelector('#popular-movies')
-    console.log(results);
+    // console.log(results);
     results.forEach(movie => {
-        console.log(movie.popular);
+        // console.log(movie.popular);
         const div = document.createElement('div')
         div.classList.add('card')
         div.innerHTML = 
@@ -109,6 +109,76 @@ async function displayPopularShows(){
     })
 }
 
+// get movie detail
+async function displayMovieDetails(id){
+    const movieId = window.location.search.split('=')[1]
+    const movie = await fetchData(`https://api.themoviedb.org/3/movie/${movieId}?`)
+
+
+
+    // const x = movie.genres.map(genre => `<li>${genre.name}</li>`)
+    // console.log(x);
+    console.log(movie);
+
+
+    const div = document.createElement('div')
+    div.innerHTML = 
+    `
+        <div class="details-top">
+        <div>
+        <a href="tv-details.html?id=${movie.id}">
+        ${movie.poster_path ? 
+            `
+                <img
+                src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.original_name}"/>
+            `: 
+            `
+                <img
+                src="..images/no-image.jpg"
+                class="card-img-top"
+                alt="${movie.original_name}"/>
+
+            `
+        }
+        </div>
+        <div>
+        <h2>${movie.original_title}</h2>
+        <p>
+            <i class="fas fa-star text-primary"></i>
+            ${movie.vote_average.toFixed(1)} / 10
+        </p>
+        <p class="text-muted">Release Date: ${movie.release_date}</p>
+        <p>
+            ${movie.overview}
+        </p>
+        <h5>Genres</h5>
+        <ul class="list-group">
+            ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
+        </ul>
+        <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+        </div>
+    </div>
+    <div class="details-bottom">
+        <h2>Movie Info</h2>
+        <ul>
+        <li><span class="text-secondary">Budget:</span> $${movie.budget}</li>
+        <li><span class="text-secondary">Revenue:</span> $${movie.revenue}</li>
+        <li><span class="text-secondary">Runtime:</span> ${movie.runtime}</li>
+        <li><span class="text-secondary">Status:</span> ${movie.budget}</li>
+        </ul>
+        <h4>Production Companies</h4>
+        <div class="list-group">${movie.production_companies.map(company => `${company.name}`).join(' ')}</div>
+    </div>
+    `
+    document.querySelector('#movie-details').appendChild(div)
+
+}
+
+
+
+
 //add spinner
 function showSpinner(){
     document.querySelector('.spinner').classList.add('show')
@@ -124,15 +194,16 @@ function hideSpinner(){
 //init app
 function init(){
     if (global.currentPage === '/' || global.currentPage === '/index.html') {
-        console.log('home');
+        // console.log('home');
         displayPopularMovies()
     }
     if (global.currentPage === '/shows.html') {
-        console.log('shows');
+        // console.log('shows');
         displayPopularShows()
     }
     if(global.currentPage ==='/movie-details.html'){
-        console.log('movie detail');
+        // console.log('movie detail');
+        displayMovieDetails()
     }
     if(global.currentPage ==='/tv-details.html'){
         console.log('tv detail');
