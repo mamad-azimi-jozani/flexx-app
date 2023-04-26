27ba1e02ed678a7f6dab5692dddd66b0c4fb5e5a
mohammad-azimi <azimi.jozani.mohammad@gmail.com>
2023-04-26 08:38:14 +0330

    add swiper

--- js/script.js
+++ js/script.js
@@ -13,6 +13,59 @@ async function fetchData(url){
     return response.json()
 }
 
+
+
+//display slider movies
+async function displaySlider(){
+    const url = `https://api.themoviedb.org/3/movie/now_playing?`
+    const { results } = await fetchData(url)
+    console.log(results);
+
+    results.forEach(movie => {
+        const div = document.createElement('div')
+        div.classList.add('swiper-slide')
+
+        div.innerHTML = 
+        `
+            <a href="movie-details.html?id=${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="Movie Title" />
+            </a>
+            <h4 class="swiper-rating">
+                <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+        `
+        // console.log(div);
+        document.querySelector('.swiper-wrapper').appendChild(div)
+        initSwiper()
+
+    })
+    console.log(document.querySelector('.swiper-wrapper'));
+}
+// displaySlider()
+function initSwiper() {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      freeMode: true,
+      loop: true,
+      autoplay: {
+        delay: 8000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        500: {
+          slidesPerView: 2,
+        },
+        700: {
+          slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
+
 //highlight active link
 function highlightActiveLink(item){
     const links = document.querySelectorAll('.nav-link')
@@ -286,6 +339,7 @@ function init(){
     if (global.currentPage === '/' || global.currentPage === '/index.html') {
         // console.log('home');
         displayPopularMovies()
+        displaySlider()
     }
     if (global.currentPage === '/shows.html') {
         // console.log('shows');
